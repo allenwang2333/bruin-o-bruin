b9d283a001685e33c244b65e161db0e4d905a807
Allen <allenwang2333@gmail.com>
2022-11-09 11:32:57 -0800

    add support to insert username and passwd and query by username

--- bruin-o-bruin/sqlite.js
+++ bruin-o-bruin/sqlite.js
@@ -1,3 +1,10 @@
+/*
+    Handles the request of database query
+    @param {string} table - the table name
+    @author Allen Wang
+    @date Nov 8 2022
+*/
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/db.sqlite', (err) => {
     if (err) {
@@ -10,11 +17,9 @@ var schema = {
     "users": ["id", "username", "passwd"]
 };
 
-/* 
-    Read all info from the table
-    However, This function is synchronous. It's weird.
-    Author: Allen Wang Nov 8 2022
-*/
+
+// Read all info from the table
+// However, This function is synchronous. It's weird.
 function readTableAll(table, callback) {
     var queryString = "SELECT * FROM " + table;
     let data = {};
@@ -31,11 +36,53 @@ function readTableAll(table, callback) {
     return data;
 }
 
+// return user name and passed for table
+function readTableByUsernames(table, username, callback) {
+    var queryString = 
+        "SELECT * FROM " + table + 
+        " WHERE username == \"" + username + "\"";
+    let userInfo = {}; 
+    db.all(queryString, (err, rows) => {
+        if (err) throw err;
+        rows.forEach(function (row) {
+            userInfo[schema[table][1]] = row[schema[table][1]];
+            userInfo[schema[table][2]] = row[schema[table][2]];
+        });
+        callback(userInfo);
+    });
+}
+
+function addUser(table, username, passwd, callback) {
+        var testExist = 
+        "SELECT * FROM " + table + 
+        " WHERE username == \"" + username + "\"";
+        var queryString = 
+        "INSERT INTO " + table + 
+        " (username, passwd) VALUES (\"" + username + "\", \"" + passwd + "\") ";
+        var flag = false;
+        db.all(testExist, (err, rows) => {
+            if (err) throw err;
+            if (rows.length > 0) {
+                flag = false;
+                console.log("User \"" + username + "\" already exists");
+                return;
+            }
+            else {
+                db.run(queryString, (err) => {
+                    if (err) throw err;
+                    callback(username);            
+                }); 
+            }
+        });        
+}
+
 function print(data) {
     console.log(data);
 }
 
 readTableAll("users", print);
+//readTableByUsernames("users", "allen", print);
+addUser("users", "jack", "313", print);
 
 db.close();
 console.log("Database connection closed");
