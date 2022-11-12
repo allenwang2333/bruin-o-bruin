a7420d5900ab4480aae0e9468bd8a15b47547c16
AaXDing <adairnogn@gmail.com>
2022-11-11 22:35:55 -0800

    Fix board's rendering onClick function call

--- bruin-o-bruin/src/gamePlay/board.js
+++ bruin-o-bruin/src/gamePlay/board.js
@@ -14,7 +14,7 @@ class Board extends React.Component{
         var board = this.state.board;
         for(var i = 0; i < board.length; i++){
             res +=  <div className="layer">
-                        <Layer layer={board[i]}/>
+                        <Layer layer={board[i]} onClick={this.props.onClick()}/>
                     </div>
         }
 
