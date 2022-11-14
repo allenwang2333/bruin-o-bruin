0ae358cf36a9867a59ec6212d820d681fd3dd984
Allen <allenwang2333@gmail.com>
2022-11-14 15:07:15 -0800

    finish user auth login via sql info

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var sqlite_db = require('./sqlite.js');
+const schema = require('./sqlite.js');
+
+console.log(schema.schema);
+const db = new sqlite_db.QueryDatabase('./db/db.sqlite', schema.schema);
 
 app.use(express.urlencoded());
 
@@ -10,9 +15,23 @@ app.post('/post', function (req, res) {
 })
 
 app.post('/server_auth_signin', function (req, res) {
+  var table = "users";
   var user_email = req.body.email;
   var user_password = req.body.password;
   console.log("Connected to react page");
+  db.connectDatabase();
+  console.log(schema);
+  db.readTableByEmail("users", user_email, function (userInfo) {
+    console.log(userInfo);
+    if (userInfo.email == user_email && userInfo.passwd == user_password) {
+      res.send('Hello POST');
+      // TODO: redirect to home page
+    }
+    else {
+      res.send('Wrong password');
+      // TODO: redirect to login page
+    }
+  });
 })
 
 app.post('/server_auth_signup', function (req, res) {
@@ -21,6 +40,16 @@ app.post('/server_auth_signup', function (req, res) {
   var user_password = req.body.password;
   console.log("Connected to react page");
   console.log(user_name, user_email, user_password);
+  db.connectDatabase();
+  db.addUser("users", user_email, user_name, user_password, function (userInfo) {
+    console.log(userInfo);
+    if (Object.keys(userInfo).length === 0) {
+      res.send('The email is already registered');
+    }
+    else {
+      res.send('Successfully registered');
+    }
+  });
 })
 
 const PORT = process.env.PORT || 8080;
