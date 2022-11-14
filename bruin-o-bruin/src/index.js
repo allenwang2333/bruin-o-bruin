b65874c792b6ff479145abf5a8439d86b69d362d
Jack He <jackhe313@g.ucla.edu>
2022-11-14 13:18:43 -0800

    change index

--- bruin-o-bruin/src/index.js
+++ bruin-o-bruin/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Game from "./gamePlay/game.js"
-import App from "./App.js"
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>
 );
 
