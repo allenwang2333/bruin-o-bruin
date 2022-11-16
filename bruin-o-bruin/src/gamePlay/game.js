7f6256ca922bd583cb05882d7c819a6e4dc80ab6
James Jin <james.jin7012@gmail.com>
2022-11-15 23:22:01 -0800

    Fix bug in displaying with the new json structure
    Modify old file: game.js
    Change the way the board variable is created using the new json
    structure and fix importing as well as displaying issues in game.js.

--- bruin-o-bruin/src/gamePlay/game.js
+++ bruin-o-bruin/src/gamePlay/game.js
@@ -2,7 +2,6 @@ import Board from "./board.js"
 import Hand from "./hand.js"
 import randomPlace from "./randomPlace.js"
 import React from "react"
-import Layout from "./layout.json"
 
 class Game extends React.Component{
     constructor(){
@@ -48,40 +47,9 @@ class Game extends React.Component{
                 parent: null,
                 child: null,
             }))
-        */
-        const cLayout = () => JSON.parse(JSON.stringify(Layout));
-        const layer0 = [cLayout[0].layer0[0].row0[0], 
-                        cLayout[0].layer0[0].row1[0], 
-                        cLayout[0].layer0[0].row2[0], 
-                        cLayout[0].layer0[0].row3[0], 
-                        cLayout[0].layer0[0].row4[0],
-                        cLayout[0].layer0[0].row5[0],
-                        cLayout[0].layer0[0].row6[0]];
-        const layer1 = [cLayout[0].layer1[0].row0[0],
-                        cLayout[0].layer1[0].row1[0],
-                        cLayout[0].layer1[0].row2[0],
-                        cLayout[0].layer1[0].row3[0],
-                        cLayout[0].layer1[0].row4[0],
-                        cLayout[0].layer1[0].row5[0]];
-        const layer2 = [cLayout[0].layer2[0].row0[0],
-                        cLayout[0].layer2[0].row1[0],
-                        cLayout[0].layer2[0].row2[0],
-                        cLayout[0].layer2[0].row3[0],
-                        cLayout[0].layer2[0].row4[0]];
-        const layer3 = [cLayout[0].layey3[0].row0[0],
-                        cLayout[0].layer3[0].row1[0],
-                        cLayout[0].layer3[0].row2[0],
-                        cLayout[0].layer3[0].row3[0]];   
-        const layer4 = [cLayout[0].layey4[0].row0[0],
-                        cLayout[0].layer4[0].row1[0],
-                        cLayout[0].layer4[0].row2[0],
-                        cLayout[0].layer4[0].row3[0]];
-        const layer5 = [cLayout[0].layey5[0].row0[0],
-                        cLayout[0].layer5[0].row1[0],
-                        cLayout[0].layer5[0].row2[0],
-                        cLayout[0].layer5[0].row3[0]];   
-
-        var board = [layer0, layer1, layer2, layer3, layer4, layer5];
+        */  
+        const cLayout = require("./layout.json")
+        var board = cLayout.board;
         console.log(board);
         const seen = this.checkSeen(board);
         this.state = {
