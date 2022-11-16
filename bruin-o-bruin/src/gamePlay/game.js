0eba1d5e7d83b34c039cd5724471da58b43e2c1d
James Jin <james.jin7012@gmail.com>
2022-11-15 22:12:10 -0800

    Implement game's board with the new json structure
    Modify old file: game.js

--- bruin-o-bruin/src/gamePlay/game.js
+++ bruin-o-bruin/src/gamePlay/game.js
@@ -2,51 +2,87 @@ import Board from "./board.js"
 import Hand from "./hand.js"
 import randomPlace from "./randomPlace.js"
 import React from "react"
+import Layout from "./layout.json"
 
 class Game extends React.Component{
     constructor(){
         super();
+        /*
         const layer0 = Array(6).fill(Array(6).fill({
                 layer: 0,
-                col: null,
                 row: null,
+                col: null,
                 category: null,
                 parent: null,
                 child: null,
             }))
         const layer1 = Array(5).fill(Array(5).fill({
                 layer: 1,
-                col: null,
                 row: null,
+                col: null,
                 category: null,
                 parent: null,
                 child: null,
             }))
         const layer2 = Array(4).fill(Array(4).fill({
                 layer: 2,
-                col: null,
                 row: null,
+                col: null,
                 category: null,
                 parent: null,
                 child: null,
             }))
         const layer3 = Array(3).fill(Array(3).fill({
                 layer: 3,
-                col: null,
                 row: null,
+                col: null,
                 category: null,
                 parent: null,
                 child: null,
             }))
         const layer4 = Array(3).fill(Array(3).fill({
                 layer: 4,
-                col: null,
                 row: null,
+                col: null,
                 category: null,
                 parent: null,
                 child: null,
             }))
-        var board = [layer0, layer1, layer2, layer3, layer4];
+        */
+        const cLayout = () => JSON.parse(JSON.stringify(Layout));
+        const layer0 = [cLayout[0].layer0[0].row0[0], 
+                        cLayout[0].layer0[0].row1[0], 
+                        cLayout[0].layer0[0].row2[0], 
+                        cLayout[0].layer0[0].row3[0], 
+                        cLayout[0].layer0[0].row4[0],
+                        cLayout[0].layer0[0].row5[0],
+                        cLayout[0].layer0[0].row6[0]];
+        const layer1 = [cLayout[0].layer1[0].row0[0],
+                        cLayout[0].layer1[0].row1[0],
+                        cLayout[0].layer1[0].row2[0],
+                        cLayout[0].layer1[0].row3[0],
+                        cLayout[0].layer1[0].row4[0],
+                        cLayout[0].layer1[0].row5[0]];
+        const layer2 = [cLayout[0].layer2[0].row0[0],
+                        cLayout[0].layer2[0].row1[0],
+                        cLayout[0].layer2[0].row2[0],
+                        cLayout[0].layer2[0].row3[0],
+                        cLayout[0].layer2[0].row4[0]];
+        const layer3 = [cLayout[0].layey3[0].row0[0],
+                        cLayout[0].layer3[0].row1[0],
+                        cLayout[0].layer3[0].row2[0],
+                        cLayout[0].layer3[0].row3[0]];   
+        const layer4 = [cLayout[0].layey4[0].row0[0],
+                        cLayout[0].layer4[0].row1[0],
+                        cLayout[0].layer4[0].row2[0],
+                        cLayout[0].layer4[0].row3[0]];
+        const layer5 = [cLayout[0].layey5[0].row0[0],
+                        cLayout[0].layer5[0].row1[0],
+                        cLayout[0].layer5[0].row2[0],
+                        cLayout[0].layer5[0].row3[0]];   
+
+        var board = [layer0, layer1, layer2, layer3, layer4, layer5];
+        console.log(board);
         const seen = this.checkSeen(board);
         this.state = {
             board: board,
