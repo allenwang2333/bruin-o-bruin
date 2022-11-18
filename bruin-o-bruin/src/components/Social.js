1d1d669a0069ba6fb35536ca7db3306278e7ff93
Jack He <jackhe313@g.ucla.edu>
2022-11-17 22:23:15 -0800

    add write post page

--- bruin-o-bruin/src/components/Social.js
+++ bruin-o-bruin/src/components/Social.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { Button, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import logo from "./assets/joe_bruin.png";
 import Posts from "./LoadPosts";
-import {AiFillEdit} from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
 
 import styles from "./SocialStyle.css";
 
@@ -43,15 +45,26 @@ function Social() {
                 <Col md={2}>
                     <Nav className={styles.navContainer}>
                         <ul className="list-group">
-                            <Nav.Link>
-                                <Link to="compose" className="text-decoration-none">
-                                    <li className="list-group-item fs-5 py-3 text-success shadow">
-                                        <span>
-                                            <AiFillEdit /> Write Post
-                                        </span>
-                                    </li>
-                                </Link>
-                            </Nav.Link>
+                            <li className="list-group-item fs-5 py-3 text-success shadow">
+                                <Popup trigger={<span> <AiFillEdit /> Write Post </span>} position="left center">
+                                    <div><div className="modal">
+                                        <div className="form-area">
+                                            <form role="form">
+                                                <br styles="clear:both" />
+                                                <div className="header">
+                                                    <input type="text" className="form-control" id="title" name="title" placeholder="Title" required />
+                                                </div>
+
+                                                <div className="form-group">
+                                                    <textarea className="form-control" type="textarea" id="subject" placeholder="Subject" maxlength="140" rows="7"></textarea>
+                                                </div>
+
+                                                <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Add Post</button>
+                                            </form>
+                                        </div>
+                                    </div></div>
+                                </Popup>
+                            </li>
                         </ul>
                     </Nav>
                 </Col>
