e457980fb8a16f0b569d89cfb1783afe1970152b
Jack He <jackhe313@g.ucla.edu>
2022-11-18 15:09:58 -0800

    fetch posts from server every second

--- bruin-o-bruin/src/components/LoadPosts.js
+++ bruin-o-bruin/src/components/LoadPosts.js
@@ -1,22 +1,37 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Posts.css";
 import Post from "./Post";
+import axios from "axios";
 var ReactDOM = require('react-dom');
 
 var username = sessionStorage.getItem("userName");
 
 const Posts = () => {
-    const blogPosts = [];
-            
+  let [blogPosts, setPosts] = useState([])
+  async function getPosts() {
+    const params = new Headers();
+    params.append('username', username);
+    const response = await axios.get('http://localhost:8080/posts', params);
+    console.log(response.data);
+    if(response.data[0].valid){
+      setPosts(response.data.slice(1));
+    } else {
+      alert(response.data[1].message);
+    }
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {getPosts();}, 1000);//refreshes every 1 second
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <div className="posts-container">
-            <h1>Posts</h1>
-        {blogPosts.map((post, index) => (
-                <Post key={index} index={index} post={post} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="posts-container">
+      <h1>Posts</h1>
+      {blogPosts.map((post, index) => (
+        <Post key={index} index={index} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
