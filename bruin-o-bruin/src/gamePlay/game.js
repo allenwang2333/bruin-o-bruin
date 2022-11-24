059cf038b036e17f03ac358275fe927e38482b04
AaXDing <adairnogn@gmail.com>
2022-11-23 23:21:37 -0800

    Add unclick feature for win display

--- bruin-o-bruin/src/gamePlay/game.js
+++ bruin-o-bruin/src/gamePlay/game.js
@@ -1,26 +1,11 @@
 import Board from "./board.js"
 import Hand from "./hand.js"
 import React from "react"
-import axios from "axios";
-import "./gamePlay.css"
 import randomPlaceBlock from "./randomPlace.js"
 import LooseDisplay from "./loosePage.js"
 import WinDisplay from "./winPage.js"
-
-async function handleSuccess() {
-    const params = new URLSearchParams();
-    params.append('author_name', sessionStorage.getItem("userName"));
-    params.append('author_id', sessionStorage.getItem("userID"));
-    const response = await axios.post('http://localhost:8080/success', params);
-    if (response.data[0].valid) {
-        alert("You won!")
-        window.setTimeout(function () {
-            window.location.href = "/home";
-        }, 1500);
-    } else {
-        alert(response.data[1].message);
-    }
-}
+import handleSuccess from "./handleSuccess.js";
+import "./gamePlay.css"
 
 class Game extends React.Component {
     constructor() {
--- bruin-o-bruin/src/gamePlay/winPage.js
+++ bruin-o-bruin/src/gamePlay/winPage.js
@@ -4,7 +4,7 @@ import Popup from 'reactjs-popup';
 class WinDisplay extends React.Component {
     render() {
         return (
-            <Popup open={this.props.win} position="center" className='popup'>
+            <Popup open={this.props.win} position="center" closeOnDocumentClick={false} closeOnEscape={false} className='popup'>
                 <div className='message'>
                     <p>
                         You Win!
