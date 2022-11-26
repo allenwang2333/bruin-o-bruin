fdf3a7c3838226303a59b5a253af72fdc29430af
Jack He <jackhe313@g.ucla.edu>
2022-11-26 15:34:34 -0800

    finished

--- bruin-o-bruin/src/components/DashBoard.js
+++ bruin-o-bruin/src/components/DashBoard.js
@@ -1,41 +1,37 @@
-import React, { useEffect } from "react";
-import {Link, useNavigate} from "react-router-dom";
-
-import Container from "react-bootstrap/Container";
+import React from "react";
+import { Link } from "react-router-dom";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-
-import {BsFillPersonPlusFill} from "react-icons/bs";
-import {IoGameControllerOutline} from "react-icons/io5";
-import {RiLoginBoxLine} from "react-icons/ri";
-
+import { IoGameControllerOutline } from "react-icons/io5";
+import { RiLoginBoxLine } from "react-icons/ri";
 import styles from "./homepage.css";
-
 import Logo from "./assets/joe_bruin.png";
-import sheep from "./assets/sheep.png";
 
 function DashBoard() {
   return (
-    <div className="container-fluid">
-        <Col className={styles.colContainer}>
-          <div className={styles.colWithButtons}>
-          <div class="positionedContainer">
-            <img src={Logo} alt="PSN logo" width={200} className={"bruinLOGO"} />
+    <div>
+      <div className="container-fluid">
+        <div >
+          <div class="center-image">
+            <img src={Logo} alt="PSN logo" width={250} />
           </div>
-            <br />
-            <Row>
-              <h2 className="text-info mb-3">Join Bruin O' Bruin today</h2>
-            </Row>{" "}
-            <br />
-            <Col>
-              <Link to="/auth" className={styles.linkTextFormat}><Button variant="success" className={`${styles.btnHomePage} mb-3`}>Sign In <RiLoginBoxLine /><BsFillPersonPlusFill /></Button></Link>
-            </Col>
-            <Col>
-              <Link to="/game" className={styles.linkTextFormat}><Button variant="success" className={`${styles.btnHomePage} mb-3`}>play as guest <IoGameControllerOutline /></Button></Link>
-            </Col>
+          <br />
+          <Row>
+            <h2 className="display">Join Bruin O' Bruin today</h2>
+          </Row>
+          <br />
+          <div class="center-button1">
+            <Link to="/auth" className={styles.linkTextFormat}><Button variant="success" className={` btn btn-primary pull-right`}>Sign In <RiLoginBoxLine /></Button></Link>
           </div>
-        </Col>
+          <br />
+          <div class="center-button2">
+          <Link to="/game" className={styles.linkTextFormat}><Button variant="success" className={` btn btn-primary pull-right`}>play as guest <IoGameControllerOutline /></Button></Link>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <p>© 2022 FALL CS35L Team 47</p>
+      </div>
     </div>
   );
 }
